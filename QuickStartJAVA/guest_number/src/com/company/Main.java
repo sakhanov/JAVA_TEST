f95dee7eb2dacabc526faf7c2b24cbe66365b878
sakhanov <sakhanov@yandex.ru>
2019-03-23 16:57:08 +0300

    Поймай каплю

--- QuickStartJAVA/guest_number/src/com/company/Main.java
+++ QuickStartJAVA/guest_number/src/com/company/Main.java
@@ -3,17 +3,22 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Игра угадай число\n Введите диапазон чисел");
-        Scanner scanner = new Scanner(System.in);
-        int range = scanner.nextInt();
+        for(int range = 10; range <= 30; range += 10){
         int MyNumber = (int)(Math.random()* range);
+        playlevel(range, MyNumber);
+        }
+        System.out.println("Игра угадай число");
+    scanner.close();
+    }
+    private static  void playlevel(int range, int MyNumber){
         int step = 0;
         while(true){
             step ++;
-            System.out.println("Введите число");
+            System.out.println("Введите число  от 0 " + range);
             int TestNumber = scanner.nextInt();
             if( TestNumber == MyNumber){
                 System.out.println("Вы угадали число за " + step + " шагов");
